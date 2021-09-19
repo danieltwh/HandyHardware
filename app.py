bbca4ef75f3aa4ce214634e1c51959e4bffa1c01
danieltwh <t.danielwh@gmail.com>
2021-09-20 00:42:06 +0800

    Login and Signup Page for Customer & Admin

---
+++ app.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from typing import Match
+from PIL import ImageTk, Image
+import sqlite3
+from login import Login_Page
+
+
+#print(TkVersion)
+
+class App(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(Login_Page)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack(side="top", fill="both", expand=True)
+
+
+
+def main():
+    # root = Tk()
+    # root.title("OSHE")
+    # # root.iconbitmap('coffee.ico')
+    # root.geometry("800x800")
+    # # app = Signup_Page(root)
+    # # app = Login_Page(root)
+    # root.mainloop()
+
+    app = App()
+    app.geometry("800x800")
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
