bae569431c8e549703bd44bbacc88e35ea77ea18
hewtungyuen <hewtungyuen@gmail.com>
2021-09-21 13:48:17 +0800

    rename branch

--- customer_request.py
+++ customer_request.py
@@ -35,31 +35,44 @@ class Request_Page(Frame):
         self.master = master
 
         new_request_btn = Button(self, text="Make a new request", command=lambda: master.switch_frame(Request_Submission_Page))
-        new_request_btn.grid(row=1, column=0, padx=(10,0), sticky="W")
+
 
         request_details_btn = Button(self, text="Request details", command=lambda: master.switch_frame(Request_Details_Page))
-        request_details_btn.grid(row=2, column=0, padx=(10,0), sticky="W")
+
 
         # returns to the catalogue page  
         back_btn = Button(self, text="Back", command=lambda: master.switch_frame(Request_Page))
-        back_btn.grid(row=10, column=0, padx=(10,0), sticky="W")
+
 
         # dropdown menu for filtering by service status 
         clicked = StringVar()
         clicked.set('NA')
 
         dropdown_label = Label(self, text="Request status")
-        dropdown_label.grid(row=4, column=0, pady=(10, 0))
 
         dropdown = OptionMenu(self, clicked, 'NA', 'SUBMITTED', 'WAITING FOR PAYMENT', 'IN PROGRESS', 'APPROVED', 'CANCELLED')
-        dropdown.grid(row=5, column=0, padx=(10,0))
 
+        # shows items according to filter specified 
         def show():
-            fliter_label = Label(self, text = clicked.get())
-            fliter_label.grid(row=11, column=0, padx=(10,0))
 
+            conn = sqlite3.connect('items.db')
+
+            c = conn.cursor()
+
+            c.execute('''
+                SELECT * FROM items
+                WHERE ServiceStatus = ?
+            ''', [''])
+
+            for record in c.fetchall():
+                print(record)
+
+            conn.commit()
+            conn.close()
+
+        # button to filter by service status
         filter_btn = Button(self, text="FILTER", command = show)
-        filter_btn.grid(row=11, column=0, padx=(10,0), sticky="W")
+
 
 # submit new request for items 
 class Request_Submission_Page(Frame):
@@ -69,7 +82,6 @@ class Request_Submission_Page(Frame):
 
         # returns to the request page  
         back_btn = Button(self, text="Back", command=lambda: master.switch_frame(Request_Page))
-        back_btn.grid(row=3, column=0, padx=(10,0), sticky="W")
 
 # viewing the request details for items that already have a request 
 class Request_Details_Page(Frame):
@@ -79,4 +91,3 @@ class Request_Details_Page(Frame):
 
         # returns to the request page  
         back_btn = Button(self, text="Back", command=lambda: master.switch_frame(Request_Page))
-        back_btn.grid(row=3, column=0, padx=(10,0), sticky="W")
